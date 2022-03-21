6bdc4593476a031f2fc8cd22f6e887a38b3b5cbe
Lisa Meyer <lisamermey@gmail.com>
2022-03-21 19:39:56 -0400

    installed body-parser library

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// converts the request body from a Buffer into a readable string
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
