f4805c11edfa67fd45e1ba5e018ecd7301a394ff
Lisa Meyer <lisamermey@gmail.com>
2022-03-22 21:11:10 -0400

    implemented logout client and server logic

--- express_server.js
+++ express_server.js
@@ -26,13 +26,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  console.log(req.cookies["username"]);
   const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = { username: req.cookies['username'] };
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -77,9 +77,16 @@ app.post('/urls/:shortURL', (req, res) => {
   res.redirect('/urls');
 });
 
+// creates a cookie to keep user logged in
 app.post('/login', (req, res) => {
   const username = req.body.username;
   res.cookie('username', username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  const username = req.body.username;
+  res.clearCookie('username');
   res.redirect('/urls');
 });
 
